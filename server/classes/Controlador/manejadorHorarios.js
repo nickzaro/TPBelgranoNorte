42ec8c9f82fc8db4f350a281ec95f8fdf14c3238
Nick <nickzaro@gmail.com>
2019-06-22 20:07:16 -0300

    funcionando buscarEnElDia

--- server/classes/Controlador/manejadorHorarios.js
+++ server/classes/Controlador/manejadorHorarios.js
@@ -1,33 +1,61 @@
 class ManejadorHorarios {
 
-    static esElHorario(hora, mapHoras) {
-        let vecHoras = mapHoras.values();
-        let encontrado = false;
-        for (let i = 0; i < vecHoras.legth - 1; i++) {//tamaño -1  porque tengo que leer el penultimo y sacar el ultimo par comparar
-            let hora1 = vecHoras[i];
-            let hora2 = vecHoras[i + 1];
-            if (this.estaEnElRango(hora, hora1, hora2)) {
-                encontrado = true;
+    static buscarEnElDia(hora, idOrigen, idDestino, mapMapHorario) {
+        console.log(mapMapHorario);
+        for (let [clave,map] of mapMapHorario) {
+            
+            if(this.esElHorario(hora,idOrigen,idDestino,map)){
+                console.log(map);
+                return true;
+            }
+        }
+    }
+    // recibe la hora del sistema, estacion actual del cliente, Estacion a donde se dirige, 
+    static esElHorario(hora, idOrigen, idDestino, unMapHorario) {
+        if (!unMapHorario.has(idOrigen) || !unMapHorario.has(idDestino)) { // si no esta el origen y destino en el mapa
+            return false;
+        }
 
+        let vecHoras = [];
+        let it = unMapHorario.entries();
+        for (let i = 0; i < unMapHorario.size; i++) {
+            vecHoras.push(it.next().value);
+        }
+
+
+        console.log(vecHoras);
+        let encontrado = false;
+        for (let i = 0; i < vecHoras.length - 1; i++) {//tamaño -1  porque tengo que leer el penultimo y sacar el ultimo par comparar
+            let hora1 = vecHoras[i][1];
+            let hora2 = vecHoras[i + 1][1];
+
+
+            if (this.estaEnElRango(hora, hora1, hora2) && vecHoras[i + 1][0] === idOrigen) { // si esta en el rango, y es idOrigen es igual a la proxima estacion
+
+                encontrado = true;
+                console.log("i: ", i, " ENCONTRADO: ",encontrado, " LA HORA DE LA ESTACION ANTERIOR es:", hora1, "  LA HORA ACTUAL", hora, "  LA HORA DE LLEGADA ES:", hora2);
+                return encontrado;
             }
 
         }
+        //console.log("Fue encontrado: ",encontrado,"LA HORA DE ARRIBO es:",hora1, "  LA HORA DE LLEGADA ES:", hora2);
         return encontrado;
     }
-    // compara si la hora actual esta en el rango
-    static estaEnElRango(dateHora,horaI, horaF) {
+    // compara si la hora actual (tiene que estar en UTC ver test ) esta en el rango
+    static estaEnElRango(dateHora, horaI, horaF) {
         let [hi, mi] = this.parsearHora(horaI);
         let [hf, mf] = this.parsearHora(horaF);
         //let [h, m] = this.parsearHora(hora);
         let dateHoraI = new Date();
         let dateHoraF = new Date();
-    
+
         dateHoraI.setUTCHours(parseInt(hi), parseInt(mi));
         dateHoraF.setUTCHours(parseInt(hf), parseInt(mf));
         //dateHora.setUTCHours(parseInt(h), parseInt(m));
         console.log("esta en el rango:", dateHoraI);
         //Cuando quisieramos obtener la hora exacta
-        dateHora.setUTCHours(dateHora.getHours()); // hora actual de sistema con correccion -3 en argentina
+
+
         console.log("hora actual:", dateHora);
         if (dateHoraF < dateHoraI) {
             dateHoraF.setDate(dateHoraF.getDate() + 1);
@@ -39,23 +67,14 @@ class ManejadorHorarios {
         return false;
     }
 
-    ///esta mal de alguna forma tengo que vincular el orden con el array horas
-    static compararDosHoras(horax, hora2) {
-
-        let res = 0;
-        let [h1, m1] = parsearHora(horax);
-        let [h2, m2] = parsearHora(hora2);
-        if (h1 === "23")
-
-
-            return res;
-
-
-    }
-
     static parsearHora(hora) {
         return hora.split(":");
 
+    }
+    static horaActualUTC() {
+        let fechaActual = new Date();
+        fechaActual.setUTCHours(fechaActual.getHours());
+        return fechaActual;
     }
 }
 module.exports = {
--- test/manejadorHorarios.test.js
+++ test/manejadorHorarios.test.js
@@ -1,4 +1,5 @@
 const {ManejadorHorarios} = require('./../server/classes/Controlador/manejadorHorarios');
+const {ManejadorViajes} = require('./../server/classes/Controlador/manejador.viajes');
 const expect = require('chai').expect;
 
 describe('Test para ManejadorHorarios', () => {
@@ -15,9 +16,30 @@ describe('Test para ManejadorHorarios', () => {
     it("funcion estaEnElRango",()=>{
         let fechaActual = new Date();
         let horaI = "0:10";
-        let horaF = "02:50";
+        let horaF = "23:50";
         expect(ManejadorHorarios.estaEnElRango(fechaActual,horaI, horaF)).to.true; // este no va dar a menos que marques la hora correcta
 
     })
+    it("funcion esElHorario ",()=>{
+        
+        console.log("FECHA ACTUAL:",ManejadorHorarios.horaActualUTC());
+        let vec =["19:00","19:05","19:11","19:16","19:20","19:23","19:27","19:32","19:38","19:45","19:49","19:53","19:56","20:00","20:03","20:07"];
+        let unMapHorario = new Map();
+        for(let i = 0; i<vec.length;i++){
+            unMapHorario.set(i,vec[i]);
+        }
+       // console.log("El Mapa: ", unMapHorario);
+        let idOrigen = 15;
+        let idDestino = 15;
 
+        expect(ManejadorHorarios.esElHorario(ManejadorHorarios.horaActualUTC(),idOrigen,idDestino,unMapHorario)).to.true;
+    })
+
+    
+    it("funcion buscarEnElDia",()=>{
+        let manejador = new ManejadorViajes();
+        let idOrigen = 15;
+        let idDestino = 15;
+        ManejadorHorarios.buscarEnElDia(ManejadorHorarios.horaActualUTC(), idOrigen, idDestino, manejador.viajesSHI.recorridos); 
+    })
 })
